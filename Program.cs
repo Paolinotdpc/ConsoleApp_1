2e730d5d1779def12ef41f4b3d6482093185f512
Drago82 <Drago82@DESKTOP-ACC38AQ>
2023-03-15 02:24:18 +0100

    Revert "prosecuzione esercizio da correggere"
    This reverts commit 78110cb7fe7c82cba2c1d491cddb2e1545dacc00.

--- Program.cs
+++ Program.cs
@@ -41,84 +41,49 @@ namespace ConsoleApp_1
             le properties dei 10 oggetti;
         */
 
+        //static void Main(string[] args)
+        //{
+        //    List<Games> collection = new list<Games>();
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        Games games = new Games();
+        //        games.Name = "Name" + i;
+        //        games.Year = i;
+
+        //        collection.Add(games);
+        //    }
+
+        //    //for (int i = 0; i < collection.Count; i++)
+        //    //{
+        //    //    Console.WriteLine(collection[i].Name + " " + collection[i].Year);
+        //    //}
+
+        //    foreach (Games games in collection)
+        //        Console.WriteLine(games.Name +" "+ games.Year);
+
+        /*
+         
+        - Chiedere all'utente i suoi dati anagrafici
+        (es. nome, cognome, età ....)
+        - Creare una classe che abbia le properties
+        chieste all'utente 
+        - istanziare un oggetto con i dati chiesti
+          all'utente
+        -stampare le properties su schermo dell'oggetto
+        */
+
         static void Main(string[] args)
         {
-            List<Games> collection = new list<Games>();
-            for (int i = 1; i <= 10; i++)
-            {
-                Games games = new Games();
-                games.Name = "Name" + i;
-                games.Year = i;
-
-                collection.Add(games);
-            }
-
-            //for (int i = 0; i < collection.Count; i++)
-            //{
-            //    Console.WriteLine(collection[i].Name + " " + collection[i].Year);
-            //}
-
-            foreach (Games games in collection)
-                Console.WriteLine(games.Name + " " + games.Year);
-
-            /*
-
-            - Chiedere all'utente i suoi dati anagrafici
-            (es. nome, cognome, età ....)
-            - Creare una classe che abbia le properties
-            chieste all'utente 
-            - istanziare un oggetto con i dati chiesti
-              all'utente
-            -stampare le properties su schermo dell'oggetto
-            */
-
-            //static void Main(string[] args)
-            //{
-            //    List<Anagrafica> persone = new List<Anagrafica>();
-            //    Anagrafica utente = new Anagrafica();
-            //    Get_user(Anagrafica utente),
-            //    persone.Add(utente);
-
-            //    foreach (Anagrafica utente in persone)
-            //    {
-
-            //        Console.WriteLine(utente.Name);
-            //        Console.WriteLine(utente.Surname);
-            //        Console.WriteLine(utente.Birthday);
-            //        Console.WriteLine(utente.Region);
-            //        Console.WriteLine(utente.City);
-            //        Console.WriteLine(utente.Address);
-            //        Console.WriteLine(utente.Number);
-            //        Console.WriteLine(utente.Date);
-            //    }
-
-            //}
-
-            //static object Get_user(Anagrafica utente)
-            //{
-            //    Console.WriteLine("inserirsci il nome: ");
-            //    utente.Name = Console.ReadLine();
-            //    Console.WriteLine("inserisci il cognome: ");
-            //    utente.Surname = Console.ReadLine();
-            //    Console.WriteLine("inserisci la data di nascita: ");
-            //    utente.Birthday = console.ReadLine());
-            //    Console.WriteLine("inserisci la regione di residenza: ");
-            //    utente.Region = console.readline();
-            //    Console.WriteLine("inserisci la città: ");
-            //    utente.City = Console.ReadLine();
-            //    Console.WriteLine("inserisci l'indirizzo: ");
-            //    utente.Address = Console.ReadLine();
-            //    Console.WriteLine("inserisci il numero civico: ");
-            //    utente.Number = int.Parse(Console.ReadLine());
-            //    Console.WriteLine("insisci la data di oggi: ");
-            //    utente.Date = DateTime.Parse(Console.ReadLine());
-            //    return utente;
-            //}
-
-
+            List<Anagrafica> persone = new List<Anagrafica>();
+            Anagrafica utente = new Anagrafica();
+            utente.Name = 
 
 
 
         }
+                           
+        
+       
+        
     }
 }
