771dc8a8afb9dd1d92a72c81ef6e913c1835509e
Drago82 <Drago82@DESKTOP-ACC38AQ>
2023-03-28 08:26:46 +0200

    merge2

--- Program.cs
+++ Program.cs
@@ -116,6 +116,211 @@ namespace ConsoleApp_1
         //static void Main(string[] args)
         // {
         //     List<Animals> animale = new List<Animals>();
+<<<<<<< HEAD
+
+        //     Random num = new Random();
+
+        //     int numCasuale = num.Next(1,5);
+
+        //     for (int i = 0; i < numCasuale; i++)
+        //     {
+        //         Animals animale = new Animals();
+        //               animale.razza = "Animale" + i;
+        //               animale.eta = i;
+        //         animale.color = "colore" + i;
+
+        //         animale.Add(animale);
+
+        //     };
+
+        //}
+
+        /*
+        - creare una classe Veicolo con:
+           - properties:
+               - Model
+               - Plate
+               - MaxSpeed
+           - metodi con contenuto arbitrario:
+               - Start
+               - Stop
+        - creare 2 oggetti di tipo Veicolo
+        - valorizzare le properties dei due oggetti e
+        invocarne i metodi         
+        */
+
+        //static void Main(string[] args)
+        //{
+        //    Veicolo veicolo1 = new Veicolo();
+        //    veicolo1.Model = "Panda";
+        //    veicolo1.Plate = "FG567TH";
+        //    veicolo1.MaxSpeed = 100;
+
+        //    Veicolo veicolo2 = new Veicolo();
+        //    veicolo2.Model = "Croma";
+        //    veicolo2.Plate = "FG432DF";
+        //    veicolo2.MaxSpeed = 80;
+
+        //    veicolo1.Start();
+        //    veicolo1.Stop();
+
+        //    veicolo2.Start();
+        //    veicolo2.Stop();
+        //}
+
+        /*creare una classe Animale con
+           - properties:
+               - Razza
+               - Colore
+           - metodi:
+               - Mangia
+               - Fai il verso
+         - creare 3 oggetti di tipo Animale,
+           popolarne le properties e invocarne
+           i metodi
+        */
+
+        //static void Main(string[] args)
+        //{
+
+        //    Animals animale1 = new Animals();
+        //    animale1.razza = "Ornitorinco";
+        //    animale1.color = "Marrone";
+
+        //    Animals animale2 = new Animals();
+        //    animale2.razza = "Elefante";
+        //    animale2.color = "grigio";
+
+        //    Animals animale3 = new Animals();
+        //    animale3.razza = "coccodrillo";
+        //    animale3.color = "Bianco";
+
+        //    animale1.startVoice();
+        //    animale2.startVoice();
+        //    animale3.startVoice();
+
+        //    animale1.startEat();
+        //    animale2.startEat();
+        //    animale3.startEat();
+
+
+        //}
+
+        /*
+         aggiornamento: 21.30
+         -creare:
+            - una classe Prodotto con:
+                - properties:
+                    - nome
+                    - costo
+            - una classe Distributore con:
+                - properties:
+                    - lista di oggetti di tipo Prodotto
+                - metodi:
+                    - Stampa, che stampa su schermo l'elenco
+                    dei prodotti nella lista
+        - istanziare un oggetto di tipo Distributore 
+        - istanziare 3 oggetti di tipo Prodotto e valorizzarne
+        le properties
+        - aggiungere i 3 oggetti alla lista di prodotti di Distrubutore
+        - stampare su schermo gli oggetti tramite il metodo Stampa
+        di Distrubutore
+        */
+
+        //var myObject = new MyClass();
+        //    myObject.MyList = new List<string>();
+        //  myObject.MyList.Add("elemento1");
+        // myObject.MyList.Add("elemento2");
+
+        //List<string> myList = new List<string>();
+        //myList.Add("elemento1");
+        //myList.Add("elemento2");
+        //    myList.Add("elemento3");
+        //static void Main(string[] args) 
+        //{
+
+
+        //    Prodotto prodotto1 = new Prodotto();
+        //    prodotto1.nome = "Mele";
+        //    prodotto1.costo = 100;
+
+        //    Prodotto prodotto2 = new Prodotto();
+        //    prodotto2.nome = "Pere";
+        //    prodotto2.costo = 100;
+
+        //    Prodotto prodotto3 = new Prodotto();
+        //    prodotto2.nome = "kiwi";
+        //    prodotto2.costo = 100;
+
+        //    Distributore distributore1 = new Distributore();
+
+        //    distributore1.Add = (prodotto1);
+        //    distributore1.Add = (prodotto2);
+        //    distributore1.Add = (prodotto3);
+
+        //    distributore1.StampaProdotti();
+
+
+        //}
+
+
+
+
+        /* aggiornamento alle 7.40
+         creare una classe EssereVivente con
+            properties:
+                - specie
+                - nome
+            metodi:
+                - nutriti
+         creare una classe Vertebrato : EssereVivente con:
+            - metodi:
+                - nutriti
+                - muoviti
+         creare una classe Pianta : EssereVivente con:
+            - properties:
+                - profondità radici
+            - metodi:
+                - nutriti
+         - istanziare due oggetti di tipo Vertebrato e due di tipo Pianta,
+         valorizzarne le properties e invocarne i metodi
+         */
+        /*aggiornamento alle 21.15
+    Creare una classe madre arbitraria con:
+       - una property public
+       - una property protected
+       - una property private
+       - un metodo public
+       - un metodo protected
+       - un metodo private
+    Creare una classe figlia arbitraria che eredita dalla classe madre con:
+       - una property private
+       - un metodo private
+    Creare una classe nipote arbitraria che eredita dalla classe figlia con:
+       - una property public
+    Istanziare:
+       - un oggetto di tipo classe madre
+       - un oggetto di tipo classe figlia
+       - un oggetto di tipo classe nipote
+    Valorizzare le properties degli oggetti
+    Eseguirne i metodi
+    */
+        static void Main(string[] args)
+        {
+
+            Madre Oggetto1 = new Madre();
+            Figlia Oggetto2 = new Figlia();
+            Nipote Oggetto3 = new Nipote();
+
+            Oggetto1 
+                
+        }
+        static void Main(string[] args)
+            {
+
+
+
+            }
 
         //     Random num = new Random();
 
@@ -133,6 +338,7 @@ namespace ConsoleApp_1
         //}
 
         /*esercizio Lezione del 20/03/2023
+>>>>>>> 4a044ced3a08e77e242bd0a579a2173a212a92b5
         
         
         - Simulare una lotteria:
